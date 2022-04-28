a5f711c35c7a417c1d3ee066b370d35bade52831
zhaochengxian <zcxzmzy@163.com>
2022-04-28 10:51:39 +0800

    修改返回工具字段data为msg

--- utils/respond.ts
+++ utils/respond.ts
@@ -2,7 +2,7 @@
 import { Code } from "../contants"
 export const responds = (error: any, data?: any) => {
     if (error) {
-        return { data: error, success: Code.PARAMS_ERROR }
+        return { msg: error, success: Code.PARAMS_ERROR }
     }
     return { data, success: Code.SUCCESS }
 }
